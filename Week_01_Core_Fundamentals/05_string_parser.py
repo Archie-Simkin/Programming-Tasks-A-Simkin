cd2c4a3556848fd29e32fcc3d9ba6eede8aeac4b
45989@rws.uk.net <45989@rws.uk.net>
2026-09-10 17:29:14 +0100

    Completed 05_string_parser

--- Week_01_Core_Fundamentals/05_string_parser.py
+++ Week_01_Core_Fundamentals/05_string_parser.py
@@ -12,12 +12,17 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
+sent = input("Enter a sentence:\n")
 
+def splitter(s):
+    words = []
+    splits = s.count(" ")
+    for i in range(splits):
+        pos = s.find(' ')
+        words.append(s[0:pos])
+        s = s[pos+1:len(s)]
+    words.append(s)
+    return(words, splits)
 
 if __name__ == "__main__":
-    main()
+    print(splitter(sent))
